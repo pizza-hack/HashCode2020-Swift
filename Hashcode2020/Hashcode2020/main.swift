e5a7c64477e9ddc1385f73f54fa86fa9db3fe1db
Gabriel Marro <gamagros@apptones.net>
2020-02-20 18:41:57 +0100

    first

---
+++ Hashcode2020/Hashcode2020/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hashcode2020
+//
+//  Created by Gabriel Marro on 20/02/2020.
+//  Copyright © 2020 Apptones. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
